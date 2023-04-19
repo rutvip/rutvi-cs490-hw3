18e66488a62a5b9a90864b369b7cf73f94e8da4b
rutvip <rp925@njit.edu>
2023-04-19 18:04:20 -0400

    initial commit + chapter 5

---
+++ web/src/components/Article/Article.js
@@ -0,0 +1,22 @@
+import { Link, routes } from '@redwoodjs/router'
+
+const truncate = (text, length) => {
+  return text.substring(0, length) + '...'
+}
+
+const Article = ({ article, summary = false }) => {
+  return (
+    <article className="mt-10">
+      <header>
+        <h2 className="text-xl text-blue-700 font-semibold">
+          <Link to={routes.article({ id: article.id })}>{article.title}</Link>
+        </h2>
+      </header>
+      <div className="mt-2 text-gray-900 font-light">
+        {summary ? truncate(article.body, 100) : article.body}
+      </div>
+    </article>
+  )
+}
+
+export default Article
